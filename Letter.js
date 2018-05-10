feb40d658efbfb04426dcfc98e0142376e3c035b
Sunny <sundeepkmanik@gmail.com>
2018-05-09 23:12:32 -0700

    letter.js setup

---
+++ Letter.js
@@ -0,0 +1,43 @@
+console.log("Testing Letter.js!");
+
+// creates letter
+class Letter {
+
+    constructor(letter) {
+        this.letter = letter;
+        this.guess = false;
+    }
+
+    // display
+    toString() {
+
+        if (this.guess) {
+
+            return this.letter;
+
+        } else {
+
+            return "_";
+
+        }
+
+    }
+
+    // letter comparison
+    checkLetter(character) {
+
+        if (character === this.letter) {
+
+            console.log("CORRECT.");
+            this.guess = true;
+
+        } else {
+
+            console.log("INCORRECT.");
+            this.guess = false;
+
+        }
+
+    }
+
+}
